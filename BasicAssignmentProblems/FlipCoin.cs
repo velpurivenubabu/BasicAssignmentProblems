39b510f53a76dc449913b734fdd6ab56b4ec23e4
venubabu <velpurivenubabu123@gmail.com>
2023-04-11 18:25:34 +0530

    [Venubabu] Added FlipCoin Code

---
+++ BasicAssignmentProblems/FlipCoin.cs
@@ -0,0 +1,38 @@
+﻿
+namespace BasicAssignmentProblems
+{
+    class FlipCoin
+    {
+        public static string FindPercentage(int number)
+        {
+            double HeadTotal = 0;
+            double TailTotal = 0;
+            Random random = new Random();
+            for (int i=0; i<number; i++)
+            {
+                
+                double reqNumber = random.NextDouble();
+                //double  = (coinSide * 0.1);
+                if (reqNumber < 0.5)
+                {
+                    HeadTotal++;
+
+                }
+                else
+                {
+                    TailTotal++;
+                }
+                
+            }
+            Console.WriteLine(TailTotal);
+            Console.WriteLine(HeadTotal);
+            double HeadsPersentage =((HeadTotal /number)*100);
+            double TailssPersentage =((TailTotal /number)*100);
+            
+            return "Head Total Persentage is : " + HeadsPersentage+" %" + " Tail Total persentage is : " + TailssPersentage + " %";
+
+
+
+        }
+    }
+}
--- BasicAssignmentProblems/Program.cs
+++ BasicAssignmentProblems/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BasicProgramming problems Assignment-1!");
+            Console.WriteLine("Please choose any one programe from below options");
+            Console.WriteLine("1.FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Please Enter The Number How many times you want to flip the coin");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    string v=FlipCoin.FindPercentage(number);
+                    Console.WriteLine(v);   
+                    break;
+
+            }
         }
-    }
+    }   
 }
