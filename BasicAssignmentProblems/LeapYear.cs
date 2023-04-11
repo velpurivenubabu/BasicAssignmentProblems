9d0ff12a28b87172ce9c1546b4a13725552387d2
venubabu <velpurivenubabu123@gmail.com>
2023-04-11 19:17:52 +0530

    [Venubabu]LeapYear Code is added

---
+++ BasicAssignmentProblems/LeapYear.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace BasicAssignmentProblems
+{
+    internal class LeapYear
+    {
+        public static string FindYear(int year)
+        {
+            bool a =(year%4==0 &&  year%100!=0);// checking weather the given year is divisible by 4,100 or not 
+            bool b = (year % 400 == 0);// cheking is given year is divisible by 400 or not
+            /* if only b statement true it's a leap year;
+             if only a statement true it's a leap year;
+             remaining all years are not a leap years */
+            if (a || b)
+                return year + " is a leap year ";
+
+            else
+                return year + " is not a leap year";
+
+
+        }
+    }
+}
--- BasicAssignmentProblems/Program.cs
+++ BasicAssignmentProblems/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to BasicProgramming problems Assignment-1!");
             Console.WriteLine("Please choose any one programe from below options");
-            Console.WriteLine("1.FlipCoin");
+            Console.WriteLine("1.FlipCoin\n2.LeapYear");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -15,6 +15,12 @@
                     int number = Convert.ToInt32(Console.ReadLine());
                     string persentage=FlipCoin.FindPercentage(number);
                     Console.WriteLine(persentage);   
+                    break;
+                case 2:
+                    Console.WriteLine("Please Enter The Year Number which sholud have four digits");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    string YearType = LeapYear.FindYear(year);
+                    Console.WriteLine(YearType);
                     break;
 
             }
